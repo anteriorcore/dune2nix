6018ed2052d499733b69b2f4fc6f35c1f7eb4d72
Shun Ueda <me@shu.nu>
2026-03-21 15:03:31 -0400

    feat: workspace

--- nix/dune2nix.nix
+++ nix/dune2nix.nix
@@ -15,29 +15,32 @@
     let
       inherit (self.lib) sexp;
 
-      mkDuneProject = lib.extendMkDerivation {
+      mkDuneWorkspace = lib.extendMkDerivation {
         constructDrv = stdenv.mkDerivation;
 
         extendDrvArgs =
           finalAttrs:
           {
             src,
-            duneProject ? src + "/dune-project",
-
-            # Any directory containing a dune-project file implicitly acts as a
-            # workspace root.
             duneWorkspace ? src + "/dune-workspace",
 
             # The build context. Dune supports "default" and Opam switch context,
-            # but I'm not convinced that we should support Opam switch context:
-            # if you're using Opam switch (not managing package via Dune), you
-            # probably don't want to use this library anyways.
+            # but I'm not convinced that we should support the latter: if you're
+            # using Opam switch (not managing package via Dune), you probably
+            # don't want to use this library anyways.
             #
-            # One situation where this might not be true is if someone wants to
-            # manage Dune via Opam, and rest of the packages via Dune. But that's
-            # also unlikely because the user of this library would use Nix (via
-            # devshell) to manage Dune -- so I will leave the door open but will
-            # not spend my complexity budget here.
+            # One situation where this might not be true, is when you manage
+            # Dune via Opam, and rest of the packages via Dune. But that's also
+            # unlikely because the user of this library would use Nix (via
+            # devshell) to manage Dune.
+            #
+            # Devex would also be terrible: the user would have to commit the
+            # Opam export file (generated via `opam export --freeze --full`),
+            # which is extremely hard to keep in sync within a team, because of
+            # Opam switch's impure nature.
+            #
+            # I will leave the door open, but I will not spend my complexity
+            # budget here.
             #
             # https://dune.readthedocs.io/en/stable/reference/dune-workspace/context.html
             context ? "default",
@@ -114,13 +117,11 @@
               ) (builtins.readDir duneLock)
             );
 
-            project = sexp.parseFile duneProject;
-
             jobsFlag = lib.optionalString enableParallelBuilding "-j $NIX_BUILD_CORES";
 
             # For some reason, `dune build` and `dune runtest` don't accept the
             # `--context` flag, instead, you specify the build target directory
-            # (`_build/${context}`) - I _hope_ this works, but I wouldn't be
+            # (`_build/${context}`). I _hope_ this works, but I wouldn't be
             # surprised at all even if this breaks someday. As I mentioned above
             # context support is best-effort: it's very possible that I rip this
             # out for a very minor issue.
@@ -135,8 +136,6 @@
           # Heavily inspired by:
           # https://github.com/NixOS/nixpkgs/blob/27894d0586cf031cd5b3b345b6f9676c99ca6bac/pkgs/build-support/ocaml/dune.nix
           {
-            name = sexp.scalar [ "package" "name" ] project;
-
             strictDeps = true;
 
             nativeBuildInputs = (args.nativeBuildInputs or [ ]) ++ [
@@ -179,12 +178,8 @@
 
               runHook postCheck
             '';
-          }
-          // lib.optionalAttrs (sexp.has [ "version" ] project) {
-            version = sexp.scalar [ "version" ] project;
           };
 
-        # Hate this hardcoded list but it's a very standard practice in nixpkgs.
         excludeDrvArgNames = [
           "duneProject"
           "duneWorkspace"
@@ -194,8 +189,31 @@
         ];
       };
 
+      # Directory containing a dune-project implicitly forms a Dune workspace.
+      mkDuneProject = lib.extendMkDerivation {
+        constructDrv = mkDuneWorkspace;
+        extendDrvArgs =
+          finalAttrs:
+          {
+            src,
+            duneProject ? src + "/dune-project",
+            ...
+          }:
+          let
+            project = sexp.parseFile duneProject;
+          in
+          {
+            name = sexp.scalar [ "package" "name" ] project;
+          }
+          // lib.optionalAttrs (sexp.has [ "version" ] project) {
+            version = sexp.scalar [ "version" ] project;
+          };
+
+        excludeDrvArgNames = [ "duneProject" ];
+      };
+
       scope = lib.makeScope newScope (_: {
-        inherit mkDuneProject;
+        inherit mkDuneProject mkDuneWorkspace;
       });
     in
     scope.overrideScope overrideScope;
