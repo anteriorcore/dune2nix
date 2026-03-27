fc23fd9042032c2a2437d77501070db16d42b34b
BenZuckier <ben.zuckier@anterior.com>
2026-03-27 17:27:10 -0400

    feat: meta.mainProgram

--- nix/dune2nix.nix
+++ nix/dune2nix.nix
@@ -231,18 +231,42 @@
           {
             src,
             duneProject ? src + "/dune-project",
+            binDuneFile ? (src + "/bin/dune"),
+            meta ? { },
             ...
           }:
           let
             project = sexp.parseFile duneProject;
+            # _Technically_ bin/dune is just convention, and __technically__
+            # you can have multiple "executable" stanzas.  So for "proper"
+            # compliance you'd collect all the executable and executables
+            # across all the dune files and decide which main program to
+            # pick... this is a sensible default and we can address if needed.
+            defaultMeta = lib.optionalAttrs (lib.pathExists binDuneFile) (
+              let
+                mapAttrsN =
+                  fn: n: attrs:
+                  if
+                    n <= 0 # comment to prevent line break here
+                  then
+                    attrs
+                  else
+                    builtins.mapAttrs (_: mapAttrsN fn (n - 1)) (fn attrs);
+                binDune = mapAttrsN (sexp.fromAlist) 2 (sexp.parseFile binDuneFile);
+                mainProgram = lib.head (
+                  binDune.executable.public_name or binDune.executables.public_names or [ null ]
+                );
+              in
+              lib.optionalAttrs (mainProgram != null) { inherit mainProgram; }
+            );
           in
           {
             name = sexp.scalar [ "package" "name" ] project;
+            meta = defaultMeta // meta;
           }
           // lib.optionalAttrs (sexp.has [ "version" ] project) {
             version = sexp.scalar [ "version" ] project;
           };
-
         excludeDrvArgNames = [ "duneProject" ];
       };
 
