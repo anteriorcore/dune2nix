7894f4c5265a47336b116efc246c2efd1165fbb6
Robin <robin.lewis@anterior.com>
2026-04-01 22:35:47 -0400

    feat: no fixup on deps with (build .. (patch ...))

--- tests/demo/package.nix
+++ tests/demo/package.nix
@@ -1,20 +1,3 @@
 { dune2nix }:
 
-dune2nix.mkDuneProject {
-  src = ./.;
-  # Stop-gap, but we should probably have a heuristic that disables fixup on any
-  # dependency with a patch.  Right?
-  srcOverrides =
-    final: prev:
-    lib.concatMapAttrs (
-      n: v:
-      lib.optionalAttrs (lib.hasPrefix "bin_prot" n) {
-        ${n} = v.overrideAttrs (
-          old:
-          lib.recursiveUpdate old {
-            passthru.sexp.source.copy = old.passthru.sexp.source.copy.overrideAttrs { dontFixup = true; };
-          }
-        );
-      }
-    ) prev;
-}
+dune2nix.mkDuneProject { src = ./.; }
