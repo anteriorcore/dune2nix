035a23643fc946a5a085dcae55ff2b5fe9ed3663
Robin <robin.lewis@anterior.com>
2026-04-19 13:44:48 -0400

    test: make_wrapper (postInstall regression) (#40)

---
+++ tests/make_wrapper/README.md
@@ -0,0 +1,1 @@
+Regression test for a postInstall hook using `makeWrapper`.
---
+++ tests/make_wrapper/package.nix
@@ -0,0 +1,29 @@
+{
+  diffutils,
+  dune2nix,
+  makeWrapper,
+}:
+
+dune2nix.mkDuneProject {
+  src = ./.;
+  nativeBuildInputs = [
+    diffutils
+    makeWrapper
+  ];
+  postInstall = ''
+    for b in $out/bin/*; do
+      wrapProgram "$b" --add-flag "hard-coded using makeWrapper"
+    done
+  '';
+  doInstallCheck = true;
+  expected = ''
+    [1] hard-coded using makeWrapper
+    [2] foo
+    [3] bar
+  '';
+  passAsFile = [ "expected" ];
+  installCheckPhase = ''
+    $out/bin/make_wrapper foo               bar > test.txt
+    diff -u test.txt $expectedPath
+  '';
+}
